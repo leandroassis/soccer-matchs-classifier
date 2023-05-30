4a4cb7447295bac78ee8680539ce0abef461159b
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-05-30 15:42:00 -0300

    inicialização do dataset

--- soccer_games_results_predictor.py
+++
@@ -1,8 +0,0 @@
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
-
-dataset = pd.read_excel('E:\\Projetos\\previsor-de-apostas-futebol\\datasets\\all-euro-data-2021-2022.xlsx') # Gets first sheet of the dataset
-
-print(len(dataset))
-#print(dataset.head(10))
-
